372c6d059a62c94929ab811cbb086b24f16f70ff
Dominic Pace <dpace@nhpace.net>
2017-01-22 22:52:54 -0500

    Implemented a base test to initialize a browser before every test.

---
+++ src/main/java/org/driverhq/automation/ExampleBaseTest.java
@@ -0,0 +1,33 @@
+package org.driverhq.automation;
+
+import org.drivrhq.drivr.browser.BrowserFactory;
+import org.drivrhq.drivr.browser.enums.BrowserType;
+import org.drivrhq.drivr.browser.enums.DriverRunType;
+import org.drivrhq.drivr.runnr.BaseTest;
+import org.testng.annotations.BeforeTest;
+
+/**
+ * (C) Copyright 2017 Dominic Pace (https://github.com/Dominic-Pace)
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ */
+public class ExampleBaseTest extends BaseTest {
+
+    /**
+     * Method used to initialize the browser before the test.
+     */
+    @BeforeTest
+    public void initBrowserBeforeTest() {
+        BrowserFactory browserFactory = new BrowserFactory(BrowserType.CHROME);
+        driver = browserFactory.getBrowserInstance(DriverRunType.DIRECT);
+    }
+}
